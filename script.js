9a91fa53be13bc38496093fc3a942879da2b104b
Guillermo Dejo <gdejo@itba.edu.ar>
2022-09-01 00:21:42 -0300

    Entrega 4

--- script.js
+++ script.js
@@ -58,69 +58,84 @@ console.log(arrayProductos);
 
 // Se imprimen los datos ingresados y procesados.
 
-for (var producto of arrayProductos) {
-    document.write("<ul><li><p>El producto ingresado es: " + producto.nombre + "</p></li>");
-    document.write("<li><p> La cantidad en stock del producto ingresado es: " + producto.cantidad + "</p></li>");
-    document.write("<li><p> El precio del producto es: " + producto.precio.toFixed(2) + "</p></li>");
-    document.write("<li><p> El precio del producto con IVA es: " + producto.sumarIva().toFixed(2) + "</p></li></ul><br>");
-    console.log(producto.nombre);
-    console.log(producto.cantidad);
-    console.log(producto.sumarIva().toFixed(2));
+for (let producto of arrayProductos) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `<h4> El producto ingresado es: ${producto.nombre}</h4>
+                            <p> La cantidad en stock del producto ingresado es: ${producto.cantidad}</p>
+                            <p> El precio del producto es: ${producto.precio}</p>`;
+    document.body.appendChild(contenedor);
 }
 
 // Unidades con poco stock - menos de 5 unidades
 
-var bajoStock = arrayProductos.filter(producto => producto.cantidad <=5 );
-console.log("Estos productos tienen poco stock, debe reabastecerlos: ");
-console.log(bajoStock);
-document.write("<h3> Lista de productos con poco stock (menos de 5 unidades): </h3>");
+function pocoStock () {
+    var bajoStock = arrayProductos.filter(producto => producto.cantidad <=5 );
+    console.log(bajoStock);
+    document.write("<h3> Lista de productos con poco stock (menos de 5 unidades): </h3>");
 
-for (var producto of bajoStock) {
-    document.write("<ul><li><p>Nombre: " + producto.nombre + "</p></li>");
-    document.write("<li><p>Cantidad: " + producto.cantidad + "</p></li></ul><br>");
+    for (let producto of bajoStock) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h4> Nombre: ${producto.nombre}</h4>
+                                <p> Cantidad: ${producto.cantidad}</p>`;
+        document.body.appendChild(contenedor);
+    }
 }
 
 // Unidades sin stock
 
-var sinStock = arrayProductos.filter(producto => producto.cantidad == 0 || producto.disponible == false);
-console.log(sinStock);
-document.write("<h3> Lista de productos sin Stock: </h3>");
+function noStock() {
+    var sinStock = arrayProductos.filter(producto => producto.cantidad == 0 || producto.disponible == false);
+    console.log(sinStock);
+    document.write("<h3> Lista de productos sin Stock: </h3>");
 
-for (var producto of sinStock) {
-    document.write("<ul><li><p>Nombre: " + producto.nombre + "</p></li>");
-    document.write("<li><p>Cantidad: " + producto.cantidad + "</p></li></ul><br>");
+    for (let producto of sinStock) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h4> Nombre: ${producto.nombre}</h4>
+                                <p> Cantidad: ${producto.cantidad}</p>`;
+        document.body.appendChild(contenedor);
+    }
 }
 
 // Buscador de un producto por nombre
 
-var nombreIngresado = prompt("Ingresar el producto que desea buscar");
-var prodIngresado = arrayProductos.filter(producto => producto.nombre.includes(nombreIngresado));
-console.log(prodIngresado);
-document.write("<h3> Lista de productos ingresados para busqueda: </h3>");
+function buscador() {
+    var nombreIngresado = prompt("Ingresar el producto que desea buscar");
+    var prodIngresado = arrayProductos.filter(producto => producto.nombre.includes(nombreIngresado));
+    console.log(prodIngresado);
+    document.write("<h3> Lista de productos ingresados para busqueda: </h3>");
 
-for (var producto of prodIngresado) {
-    document.write("<ul><li><p>Nombre: " + producto.nombre + "</p></li>");
-    document.write("<li><p>Precio: " + producto.precio + "</p></li></ul><br>");
+    for (let producto of prodIngresado) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h4> Nombre: ${producto.nombre}</h4>
+                                <p> Cantidad: ${producto.cantidad}</p>
+                                <p> Precio: ${producto.precio}</p>`;
+        document.body.appendChild(contenedor);
+    }
 }
 
 // Sistema de ordenado por cantidad
 
-var ordenCantidad = []; 
-ordenCantidad = arrayProductos.map(elemento => elemento);
-ordenCantidad.sort(function(a,b) {
-    return a.cantidad - b.cantidad;
-});
-console.log("Productos ordenados por cantidad ascendente: ");
-console.log(ordenCantidad);
-document.write("<h3> Lista de productos ordenados por cantidad ascendente: </h3>");
+function ordenCant(){
+    var ordenCantidad = []; 
+    ordenCantidad = arrayProductos.map(elemento => elemento);
+    ordenCantidad.sort(function(a,b) {
+        return a.cantidad - b.cantidad;
+    });
+    console.log("Productos ordenados por cantidad ascendente: ");
+    console.log(ordenCantidad);
+    document.write("<h3> Lista de productos ordenados por cantidad ascendente: </h3>");
 
-for (var producto of ordenCantidad) {
-    document.write("<ul><li><p>Nombre: " + producto.nombre + "</p></li>");
-    document.write("<li><p>Cantidad: " + producto.cantidad + "</p></li></ul><br>");
+    for (let producto of ordenCantidad) {
+        let contenedor = document.createElement("div");
+        contenedor.innerHTML = `<h4> Nombre: ${producto.nombre}</h4>
+                                <p> Cantidad: ${producto.cantidad}</p>`
+        document.body.appendChild(contenedor);
+    }
 }
 
 // Sistema de ordenado por precio
 
+function ordenPre() {
 var ordenPrecio = []; 
 ordenPrecio = arrayProductos.map(elemento => elemento);
 var ordenPrecio = arrayProductos;
@@ -132,7 +147,10 @@ console.log("Productos ordenados por precio ascendente: ");
 console.log(ordenPrecio);
 document.write("<h3> Lista de productos ordenados por precio ascendente: </h3>");
 
-for (var producto of ordenPrecio) {
-    document.write("<ul><li><p>Nombre: " + producto.nombre + "</p></li>");
-    document.write("<li><p>Precio: " + producto.precio + "</p></li></ul><br>");
+for (let producto of ordenPrecio) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `<h4> Nombre: ${producto.nombre}</h4>
+                            <p> Precio: ${producto.precio}</p>`;
+    document.body.appendChild(contenedor);
+}
 }
